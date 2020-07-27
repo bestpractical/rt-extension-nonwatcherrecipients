1e725ff4a86c6ac9a8cd6ed5894c7e67d8f5d1f2
sunnavy <sunnavy@bestpractical.com>
2020-07-28 02:32:01 +0800

    Support RT 5.0 and update docs accordingly

--- lib/RT/Extension/NonWatcherRecipients.pm
+++ lib/RT/Extension/NonWatcherRecipients.pm
@@ -55,7 +55,7 @@ in your database.
 If you are upgrading this module, check for upgrading instructions
 in case changes need to be made to your database.
 
-=item Edit your F</opt/rt4/etc/RT_SiteConfig.pm>
+=item Edit your F</opt/rt5/etc/RT_SiteConfig.pm>
 
 If you are using RT 4.2 or greater, add this line:
 
