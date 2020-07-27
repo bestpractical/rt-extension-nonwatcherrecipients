10a3f68e8bc88b9cfcd40bb3b9e98125b8dc4ba6
sunnavy <sunnavy@bestpractical.com>
2020-07-28 02:38:28 +0800

    Update copyright year to 2020

--- lib/RT/Extension/NonWatcherRecipients.pm
+++ lib/RT/Extension/NonWatcherRecipients.pm
@@ -191,7 +191,7 @@ or via the web at
 
 =head1 LICENSE AND COPYRIGHT
 
-This software is Copyright (c) 2013-2016 by Best Practical Solutions, LLC
+This software is Copyright (c) 2013-2020 by Best Practical Solutions, LLC
 
 This is free software, licensed under:
 
